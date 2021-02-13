5367809542a988b7feeb0eb2f0dc051c52bea410
Anicet Ebou <anicet.ebou@gmail.com>
2021-02-13 17:35:53 +0000

    Add contact page

--- app.py
+++ app.py
@@ -15,5 +15,10 @@ def run():
     return render_template("run.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
