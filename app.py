199d3ef2b7c32a3f2fbe12a8f421c86738885708
Anicet Ebou <anicet.ebou@gmail.com>
2021-02-13 12:15:21 +0000

    Add home page template, redis and so on

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 import os
 from flask import Flask
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+q = Queue(connection=conn)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello ConoDictor!"
+    errors = []
+    results = {}
+    if request.method == "POST":
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+        
+    return render_template('index.html', errors=errors, results=results)
+
+
 
 if __name__ == '__main__':
     app.run()
