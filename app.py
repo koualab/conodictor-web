95641aae5f2824250aafbbe6666e933b146b2c99
Anicet Ebou <anicet.ebou@gmail.com>
2021-02-13 16:09:20 +0000

    Templating webapp

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 import os
-from flask import Flask
-from rq import Queue
-from rq.job import Job
-from worker import conn
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ["APP_SETTINGS"])
 
-q = Queue(connection=conn)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-
-@app.route('/run', methods=['GET', 'POST'])
+@app.route("/run", methods=["GET", "POST"])
 def run():
-    return render_template('run.html')
+    return render_template("run.html")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-    
